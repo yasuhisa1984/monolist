1115eb1cf3c6087207e4ab71c402a4b8da2d2b34
yasuhisa1984 <penmouse275@icloud.com>
2015-09-27 10:48:38 +0000

    kadai4

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,14 +1,17 @@
 class RankingController < ApplicationController
-      def have
-     ids = Have.group(:item_id).order('count_item_id desc').limit(10).count(:item_id).keys
-    #ids = Have.joins(:item).group(:item_id).order('count_item_id desc, max(items.created_at) desc').limit(10).count(:item_id).keys
-    @items = Item.find(ids).sort_by{|o| ids.index(o.id)}
+  def have
+    ids = Have.group(:item_id).order('count_item_id desc').limit(10).count(:item_id).keys
+  
+    #findでidsを取得（この状態では並べ替え未完成）。itemのidをindexでidsに合致させ出力し、並べ替え。
+    @items = Item.find(ids).sort_by{|i| ids.index(i.id)}
+    
+    #itemのID番号
     @counts = ids
   end
 
   def want
     ids = Want.group(:item_id).order('count_item_id desc').limit(10).count(:item_id).keys
-    @items = Item.find(ids).sort_by{|o| ids.index(o.id)}
+    @items = Item.find(ids).sort_by{|i| ids.index(i.id)}
     @counts = ids
   end
 end
